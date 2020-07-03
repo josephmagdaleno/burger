c5050e6228dd0a43bd3109cc832d4b2873ccd2a3
Joseph Magdaleno <61814505+josephmagdaleno@users.noreply.github.com>
2020-07-02 21:33:58 -0700

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (burger_name) VALUES ("bacon");
-INSERT INTO burgers (burger_name) VALUES ("cheeseburger");
-INSERT INTO burgers (burger_name) VALUES ("chipotle");
+INSERT INTO burgers (burger_name) VALUES ("Hamburger");
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("VEGGIE BURGER");
 INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
